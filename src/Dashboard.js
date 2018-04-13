f4c52a2c345405715029466c7ee9226c7b63163c
admin <Muaath.Alaraj@gmail.com>
2018-04-13 08:16:54 -0700

    Dashboard

---
+++ src/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  constructor(catToAdopt, dogToAdopt) {
+    super(catToAdopt, dogToAdopt);
+    this.state = {};
+  }
+
+  render() {
+    return <div />;
+  }
+}
+
+export default Dashboard;
